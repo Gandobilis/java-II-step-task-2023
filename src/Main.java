507d125aacd8864b6a8dd58c442f245798777a62
Gandobilis <lashadeveloper@gmail.com>
2023-09-22 18:35:54 +0400

    first exercise corrected.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,23 @@
-
+import java.util.Arrays;
 
 public class Main {
-    int singleNumber(int[] nums) {
-        int result = 0;
+    public static void main(String[] args) {
 
-        for (int num : nums)
-            result ^= num;
+    }
 
-        return result;
+    public static int singleNumber(int[] nums) {
+        quickSort(nums);
+        int first = 0;
+        int last = nums.length - 1;
+
+        if (nums[first] != nums[first + 1]) return nums[first];
+        else if (nums[last] != nums[last - 1]) return nums[last];
+        else
+            for (int curr = 1; curr < nums.length - 1; curr++)
+                if (nums[curr] != nums[curr - 1] && nums[curr] != nums[curr + 1])
+                    return nums[curr];
+
+        return -1;
     }
 
     int minSplit(int amount) {
@@ -25,28 +35,9 @@ public class Main {
         return minCoins;
     }
 
-    int notContains(int[] array) {
-        int n = array.length;
-
-        for (int i = 0; i < n; i++)
-            if (array[i] <= 0 || array[i] > n)
-                array[i] = n + 1;
-
-
-        for (int i = 0; i < n; i++) {
-            int num = Math.abs(array[i]);
-            if (num <= n)
-                array[num - 1] = -Math.abs(array[num - 1]);
-
-        }
-
-        for (int i = 0; i < n; i++)
-            if (array[i] > 0)
-                return i + 1;
-
-
-        return n + 1;
-    }
+//    int notContains(int[] array) {
+//
+//    }
 
     String binarySum(String a, String b) {
         return "";
@@ -54,5 +45,38 @@ public class Main {
 
     int countVariants(int stearsCount) {
         return 0;
+    }
+
+    private static void quickSort(int[] arr) {
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    private static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+
+        for (int j = low; j <= high - 1; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+
+        swap(arr, i + 1, high);
+        return (i + 1);
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
